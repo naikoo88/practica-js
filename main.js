d09ad50b5351246574026ea8fc59f646b5fd83b9
Nicolas Colombero <nicolas.colombero@gmail.com>
2023-02-08 09:38:04 -0300

    se corrige funcionalidad de menues

--- main.js
+++ main.js
@@ -2,12 +2,19 @@ var navbarEmail = document.querySelector('.navbar-email');
 var desktopMenu = document.querySelector('.desktop-menu');
 var menuHabIcon = document.querySelector('.menu');
 var mobileMenu = document.querySelector('.mobile-menu')
+var menuCartIcon = document.querySelector('.navbar-shopping-cart');
+var productDet = document.querySelector('.product-detail');
 
 navbarEmail.addEventListener('click', toggleMenu); 
-menuHabIcon.addEventListener('click', toggleMobileMenu); 
+menuHabIcon.addEventListener('click', toggleMobileMenu);
+menuCartIcon.addEventListener('click', toggleProductMenu);
+
+var isMobileMenuClose = mobileMenu.classList.contains('inactive');
+
+
+
 
 function toggleMenu() {
-    
     // SOLUCION DE NICOLAS
     //
     // if(desktopMenu.getAttribute('class') == "desktop-menu inactive") {
@@ -15,10 +22,25 @@ function toggleMenu() {
     // } else {
     //     desktopMenu.setAttribute('class', 'desktop-menu inactive');
     // }
-
     desktopMenu.classList.toggle('inactive') 
 }
 
 function toggleMobileMenu() {
-    mobileMenu.classList.toggle('inactive') 
+    let isProductDetClose = productDet.classList.contains('inactive');
+    if(isProductDetClose){
+        mobileMenu.classList.toggle('inactive')    
+    } else {
+        productDet.classList.toggle('inactive')
+        mobileMenu.classList.toggle('inactive')    
+    }
+}
+
+function toggleProductMenu() {
+    let isMobileMenuClose = mobileMenu.classList.contains('inactive');
+    if(isMobileMenuClose){
+        productDet.classList.toggle('inactive')    
+    } else {
+        mobileMenu.classList.toggle('inactive')
+        productDet.classList.toggle('inactive')    
+    }
 }
